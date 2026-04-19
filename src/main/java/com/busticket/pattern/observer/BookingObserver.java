26dca45f19556746d61785cef6fbd341b7b24316
abdullah4295 <abdullah.10120032@gmail.com>
2026-04-20 00:08:00 +0600

    Update BookingObserver.java

--- src/main/java/com/busticket/pattern/observer/BookingObserver.java
+++ src/main/java/com/busticket/pattern/observer/BookingObserver.java
@@ -2,18 +2,9 @@ package com.busticket.pattern.observer;
 
 import com.busticket.entity.Booking;
 
-/**
- * OBSERVER PATTERN: BookingObserver Interface
- * 
- * Any class that wants to be notified when a booking is made
- * must implement this interface.
- */
+
 public interface BookingObserver {
 
-    /**
-     * Called when a new booking is successfully created.
-     *
-     * @param booking The newly created booking
-     */
+
     void onBookingCreated(Booking booking);
 }
